2cec88b88ba0568e41599143998c92cd4835a8e8
iicakeii <marccharles243@gmail.com>
2024-03-02 11:57:14 -0500

    backend server is up and is running on port 3000.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
+const LISTENING_PORT = 3000; // LISTENING PORT
+
+const app = express(); //Create another variable to initialize express
 
 // Something of convenience..Helps convert the body of reqs/reps into JSON so that 
 // it does not need to be created manually on every reqest.
@@ -9,15 +11,19 @@ app.use(express.json());
 
 app.use(cors()); //Annonying security thing
 
+//Creating Routes
+app.get('/' /*Name of the Route*/, (req /*request*/, res /*response*/) => {
+	res.send('Hello World');
+})
 
-app.get("api/recipes/search", async (req /*request*/, res /*response*/) => {
-	res.json({message: 'success'});
-});
 
-const LISTENING_PORT = 3000 // LISTENING PORT
+app.get("api/recipes/search", async (req, res) => {
+	res.json({message: 'success'}); //Use .json() to send objects
+})
+
 app.listen(LISTENING_PORT, () => {
 
 	//Code that runs after the app has successfully started.
 	console.log(`Server is listening on port: ${LISTENING_PORT}.`); 
-} );
+})
 
