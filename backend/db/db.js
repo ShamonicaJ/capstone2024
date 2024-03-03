f287fe7da1980af465c2bf3ecc112e547d9a0809
iicakeii <marccharles243@gmail.com>
2024-03-03 14:42:25 -0500

    route connected. TRY:'curl localhost:3000/api/tasks' in terminal, to test out route

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,16 @@
 import pg from 'pg';
+const DATABASE = 'capstone_db';
+const DB_PORT = 5432;
+const USER_NAME = 'iiicakeiii';
+const PASS = 'cake';
+const HOST = 'localhost';
 
 const client = new pg.Pool({ //make a connection to a pool
-	username: "iiicakeiii", //
-	password: "cake",
-	port: 5432, //port number stays the same (?)
-	database: 'capstone_db', //name of database
-	host: 'localhost', //locally hosted
+	username: USER_NAME, //
+	password: PASS,
+	port: DB_PORT, //port number stays the same (?)
+	database: DATABASE, //name of database
+	host: HOST, //locally hosted
 	
 });
 
@@ -14,6 +19,7 @@ const startDB = async () => {
 	let instance;
 	try {
 		instance = await client.connect();
+		console.log(`Successfully connected to ${DATABASE}`);
 	} catch (e) {
 		console.error('Failure connecting to database. @function startDB() ', e);
 		throw e; //kill app if can't connect
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { startDB, client } from '../db/db.js';
 import cors from 'cors';
 
 const LISTENING_PORT = 3000; // LISTENING PORT
@@ -7,27 +8,57 @@ const app = express(); //Create another variable to initialize express
 // Something of convenience..Helps convert the body of reqs/reps into JSON so that 
 // it does not need to be created manually on every reqest.
 app.use(express.json());
-
 app.use(cors()); //Annonying security thing
 
+
 //Creating Routes
-app.get('/' /*Name of the Route*/, (req /*request*/, res /*response*/) => {
+let getPath;
+
+/*Name of the Route*/
+getPath = `/`; 
+app.get(getPath, (req /*request*/, res /*response*/) => {
 
 	res.send('Hello World');
 	
 })
 
+getPath = '/api/tasks';
+app.get(getPath, async (req, res) => {
+	try {
+		const result = await client.query(`
+			SELECT * FROM tasks
+		`);
+		res.send( { tasks: result.rows } );
+	} catch (e) {
+		console.error(`ERROR @get_req:${getPath}; Failed to get tasks`, e);
+	}
+})
 
-app.get("api/recipes/search", async (req, res) => {
-
-	res.json({message: 'success'}); //Use .json() to send objects
+getPath = 'api/recipes/search';
+app.get(getPath, async (req, res) => {
+	try {
+		res.json({message: 'success'}); //Use .json() to send objects
+	} catch (e) {
+		console.error(`ERROR @get_req:${getPath}; Failed to get tasks`, e);
+	}
 
 })
 
-app.listen(LISTENING_PORT, () => {
+const startServer = async () => {
 
-	//Code that runs after the app has successfully started.
-	console.log(`Server is listening on port: ${LISTENING_PORT}.`); 
+	//TRY_BLOCK
+	try {
+		await startDB(); //Wait for the database to startup, if something goes wrong skip error output.
+		app.listen(LISTENING_PORT, () => {
+			//Code that runs after the app has successfully started.
+			console.log(`Server is listening on port: ${LISTENING_PORT}.`); 
+		})
+		
+	//ERROR_OUPUT
+	} catch (e) {
+		console.error(`Failed to start database/server @function startServer();`, e);
+		throw e;
+	}
+}
 
-})
-
+startServer();
