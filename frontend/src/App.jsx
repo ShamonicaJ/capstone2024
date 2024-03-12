450a6031244d6cd0d5c5166da0992e0236cdac9a
aking3424 <143570056+aking3424@users.noreply.github.com>
2024-03-11 21:22:18 -0400

    Frontend before connecting to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,72 @@
-import React from 'react';
 import "./App.css";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import * as api from "./api";
+import {Recipe} from "./types";
 import RecipeCard from "./components/RecipeCard";
+import RecipeModal from "./components/RecipeModal";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(undefined);
+  const pageNumber = useRef(1);
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+      pageNumber.current = 1;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleViewMoreClick = async () => {
+    const nextPage = pageNumber.current + 1;
+    try {
+      const nextRecipes = await api.searchRecipes(searchTerm, nextPage);
+      setRecipes([...recipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="app-container">
       <div className="header">
-        <img src="/koreanfood.jpg"></img>
-        <div className="title">Culinary Compass </div>
+        <img src="/koreanfood.jpg" alt="Korean Food"></img>
+        <div className="title">World Cuisine Hub</div>
       </div>
       <div className="tabs">
         <h1>Search</h1>
         <h1>Favorites</h1>
       </div>
 
-      <>
-        <form>
+      <div>
+        <form onSubmit={(event) => handleSearchSubmit(event)}>
           <input
             type="text"
             required
-            placeholder="Search for recipes..."
+            placeholder="Enter a search term ..."
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
           ></input>
-          <button type="submit">
-            <AiOutlineSearch size={40} />
-          </button>
+          <button type="submit">Submit</button>
         </form>
 
-        <div className="recipe-grid">
-          <RecipeCard />
-        </div>
-
-        <button className="view-more-button">View More</button>
-      </>
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
+        ))}
+        <button className="view-more-button" onClick={handleViewMoreClick}>
+          View More
+        </button>
+        {selectedRecipe && (
+          <RecipeModal recipeId={selectedRecipe.id.toString()} />
+        )}
+      </div>
     </div>
   );
 };
--- frontend/src/components/RecipeModal.jsx
+++ frontend/src/components/RecipeModal.jsx
@@ -1,18 +1,40 @@
-const RecipeModal = () => {
-    return (
-      <>
-        <div className="overlay"></div>
-        <div className="modal">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h2>RECIPE TITLE</h2>
-              <span className="close-btn">&times;</span>
-            </div>
-            <p>RECIPE SUMMARY</p>
+import { useEffect, useState } from "react";
+import { RecipeSummary } from "../types";
+import * as RecipeAPI from "../api";
+
+
+const RecipeModal = ({ recipeId }) => {
+  const [recipeSummary, setRecipeSummary] = useState()
+
+  useEffect(() => {
+    const fetchRecipeSummary = async () => {
+      try {
+        const summaryRecipe = await RecipeAPI.getRecipeSummary(recipeId)
+        setRecipeSummary(summaryRecipe)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchRecipeSummary()
+  }, [recipeId])
+
+  if (!recipeSummary) {
+    return <></>
+  }
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className="modal">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h2>{recipeSummary?.id}</h2>
+            <span className="close-btn">&times;</span>
           </div>
+          <p dangerouslySetInnerHTML={{ __html: recipeSummary.summary }}></p>
         </div>
-      </>
-    );
-  };
-  
-  export default RecipeModal;
+      </div>
+    </>
+  )
+}
+
+export default RecipeModal
