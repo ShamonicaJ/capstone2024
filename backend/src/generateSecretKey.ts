af8ee7c0f9f71b4511b0854ce8021363c8223071
ShamonicaJ <146474322+ShamonicaJ@users.noreply.github.com>
2024-04-11 19:34:54 -0400

    Delete backend/src/generateSecretKey.ts

--- backend/src/generateSecretKey.ts
+++
@@ -1,8 +0,0 @@
-// import crypto from 'crypto';
-
-// const generateRandomSecret = () => {
-//     return crypto.randomBytes(32).toString('hex');
-// };
-
-// const secretKey = generateRandomSecret();
-// console.log(secretKey);
